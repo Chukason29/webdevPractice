15fb3134956e4df72437d84be70020884d6b1774
Chukwuebuka Alaegbu <chukwuebukaalaegbu@gmail.com>
2023-09-27 13:13:20 +0100

    array sort added

--- Practice/js/Array.js
+++ Practice/js/Array.js
@@ -47,8 +47,11 @@ console.log(arr3);
 const arrayz = [1,2,3,4,5,6,7,8,9,10,11,12,13,14];
 
 // .every() return boolean value... if every item passes the test, it return true. if any item fails the test, 
-// it returns false
+// it returns false even if only one item doesn't pass the test
 
+let isArrayAllEven = arrayz.every((item) => item % 2 == 0);
+
+console.log(isArrayAllEven); // returns false because arrayz has odd numbers also
 
 
 // filter is used to populate another array with items that passed a test in this current array
@@ -59,3 +62,45 @@ console.log(evens);  // output all even numbers in arrays
 
 const odds = arrayz.filter( (item) => item % 2 > 0)
 console.log(odds); // output all odd numbers in arrays
+
+
+// .sort() is used to sort string data in an array in an ascending order
+
+const places = ["zoo", "congo", "Nigeria", "China", "Denmark", "Malta"];
+console.log(places.sort());
+
+// to sort strings in a descending order, then use .reverse()
+console.log(places.reverse());
+
+// You cant use the sort() just like that to sort numbers
+
+const numbers = [3,5,4,23,12,54,17,15,28,62];
+
+console.log(numbers.sort)// This wont work at all
+
+console.log(numbers.sort((x, y) => {x - y})); // this will sort in ascending order
+console.log(numbers.sort((x, y) => { y - x})); // this will sort in descending order
+
+const products = [
+    {
+        name: 'Nissan',
+        price: 6000
+    },
+    {
+        name: 'bmw',
+        price: 3000
+    },
+    {
+        name: 'benz',
+        price: 25000
+    },
+    {
+        name: 'Porsche',
+        price: 20000
+    }
+]
+
+let sortedProducts = products.sort((x, y) => {x.price - y.price});
+let lowProducts = products.filter((product) => product.price <= 10000); // selects every  products less than 10,000 dollars
+console.log(lowProducts);
+
