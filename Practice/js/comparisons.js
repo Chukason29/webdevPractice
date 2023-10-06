46dc84a2af4c4725e9230ad9a9e7af73a05fced8
Chukwuebuka Alaegbu <chukwuebukaalaegbu@gmail.com>
2023-10-06 14:50:18 +0100

    practiced js comparisons

---
+++ Practice/js/comparisons.js
@@ -0,0 +1,24 @@
+// Never you compare values of different types, They might give unexpected results
+// Always convert to same type before any comparison
+
+function checkAge(age) {
+    var votableAge;
+    if (isNaN(age)) {
+        votableAge = "This is not a number";
+    }else{
+        votableAge = (age < 18) ? "Not in age range" : "In age range";
+    }
+    return votableAge;
+}
+console.log(checkAge(17));
+
+// Sometimes you want access a property of an object that might not exist
+//instead of making it to throw an error, use the chaining operator (?.)
+
+car = {
+    model : 'AX007',
+    price : 12000,
+    color: "Yellow"
+}
+
+console.log(car?.name);
