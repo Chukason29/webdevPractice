94d7589cd06ec1610cac76767d5498d873c52687
Chukwuebuka Alaegbu <chukwuebukaalaegbu@gmail.com>
2023-10-14 14:50:10 +0100

    Practicing call method

---
+++ Practice/js/functions/apply-method.js
@@ -0,0 +1,24 @@
+// The .apply method is very to .call method. 
+//just that apply takes arrays as arguments instead of the literals or variables
+
+const superSubs = {
+    subMethod: function(averageGoal){
+        const isGreatSub = this.goalContri > averageGoal ? `${this.name} is a great sub` : `${this.name} is not a great sub`;
+        return isGreatSub;
+    }
+}
+
+const sub1 = {
+    name: "CR7",
+    goalContri: 24
+}
+const sub2 = {
+    name: "Havertz",
+    goalContri: 7
+}
+
+//the difference btw call and apply is that apply must receive arguments as an array
+let checkSub = superSubs.subMethod.call(sub2, 20);
+let checkSub2 = superSubs.subMethod.apply(sub2, [20]);
+console.log(checkSub);
+console.log(checkSub2);
