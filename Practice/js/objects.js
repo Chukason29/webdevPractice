80a4793ea5e3fa2995f7faf21262a0bd03f32ea8
Chukwuebuka Alaegbu <chukwuebukaalaegbu@gmail.com>
2023-11-21 03:37:51 +0100

    learnt about call, apply and bind

--- Practice/js/objects.js
+++ Practice/js/objects.js
@@ -1,3 +1,4 @@
+"use strict"
 //object definition Or Object creation
 const objectName =  {};
 console.log(typeof  objectName);
@@ -14,4 +15,58 @@ console.log(objectName);
 // newObject and objectName and exact samething
 // any changes made to newObject, automatically affects objectName
 const newObject = objectName;
-console.log(newObject);
+newObject.name = "Ngozi";
+console.log(objectName);// {name: Ngozi, age: 29}
+
+// objects can also be accessed using []
+console.log(objectName["name"]);
+
+// objects can also be accessed using variable name
+let myName = "name";
+console.log(objectName[myName]);
+
+
+//Iterating over an object 
+const student = {
+    fname: "Ngozi",
+    lname: "Fred",
+    age: 28,
+    color: "ebony"
+}
+
+// the for in loop is the best way to iterate over an object
+for (const key in student) {
+    console.log(
+        `${key} : ${student[key]}`
+    );
+}
+
+//deleting an object property
+delete student.color;
+console.log(student);
+
+//converting object to a map
+const newMap = new Map(Object.entries(student));
+
+for (const [key, value] of newMap) {
+    console.log(`${key} : ${value}`);
+}
+
+//Object methods
+const person1 = {
+    name: "Wisdom",
+    age: 29,
+    voteEli: function(){
+        const isEligible = this.age >= 18 ? "Yor are eligible" : "You are not eligible"
+        return isEligible
+    }
+}
+console.log(person1.voteEli());
+
+// you can access the voteEli method from another object with the call() 
+// or apply method()
+const person2 = {
+    name: "Grace",
+    age: 16
+}
+console.log(person1.voteEli.call(person2));
